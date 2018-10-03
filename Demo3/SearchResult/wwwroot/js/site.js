030cd6bd4f34182bff7dededca028adabb1a689e
shernandez <shernandez@encamina.com>
2018-10-03 11:20:47 +0200

    ....

---
+++ Demo3/SearchResult/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿function Buscar()
+{
+    var url = "http://dotnetapisearch.azurewebsites.net/api/search?index=csindex&query=" + $("#queryinput").val();
+    $.get(url, function (data)
+    {
+        var result = data;
+    });
+
+}
